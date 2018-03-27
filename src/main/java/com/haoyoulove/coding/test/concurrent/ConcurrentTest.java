4d448dee6f06c78ce370f25f1a260ab8dfe5863d
haoyoulove <584703362@qq.com>
2018-03-27 19:36:15 +0800

    测试提交

--- src/main/java/com/haoyoulove/coding/test/concurrent/ConcurrentTest.java
+++ src/main/java/com/haoyoulove/coding/test/concurrent/ConcurrentTest.java
@@ -3,7 +3,8 @@ package com.haoyoulove.coding.test.concurrent;
 public class ConcurrentTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("test");
 
 	}
 
