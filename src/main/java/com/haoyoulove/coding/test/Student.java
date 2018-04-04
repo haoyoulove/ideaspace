da259741c2f69ff2c90d6a65fed8a8204070c10f
haoyoulove <584703362@qq.com>
2018-04-04 12:35:19 +0800

    lombokTest

---
+++ src/main/java/com/haoyoulove/coding/test/Student.java
@@ -0,0 +1,17 @@
+package com.haoyoulove.coding.test;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Student {
+
+	private String name;
+	private int age;
+	private String male;
+	private String studentNo;
+	
+}
