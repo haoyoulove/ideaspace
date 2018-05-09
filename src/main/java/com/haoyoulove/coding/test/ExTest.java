1e1bef50718bb81f95e60d925b0b43d885c4f5d3
haoyoulove <584703362@qq.com>
2018-05-09 17:18:50 +0800

    test

--- src/main/java/com/haoyoulove/coding/test/ExTest.java
+++ src/main/java/com/haoyoulove/coding/test/ExTest.java
@@ -1,6 +1,9 @@
 package com.haoyoulove.coding.test;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class ExTest {
 
@@ -18,21 +21,53 @@ public class ExTest {
 
 	}
 
-	public static void main(String args[]) {
-//		methodA(0);
+	public static void test(){
+		//		methodA(0);
 //		System.out.println(j);
-		
+	}
+
+	public static void test1(){
 		String str = "abc";
 		System.out.println(str.hashCode());
-		
+
 		char c = str.charAt(0);
 		char c2 = str.charAt(1);
 		char c3 = str.charAt(2);
 		System.out.println(0+c);
 		System.out.println(97 * 31 +c2);
-		
+
 		System.out.println(3105 * 31 +c3);
+	}
+
+	public static void main(String args[]) {
+
+		List<String> strings = Arrays.asList("6", "1", "3", "1","2");
+		Collections.sort(strings);
+		for (String s : strings) {
+			System.out.println(s);
+		}
+
 	}
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
