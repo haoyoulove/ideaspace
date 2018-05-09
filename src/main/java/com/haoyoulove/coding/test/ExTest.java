611b596603df0adc701cac55d16b799a399e5867
haoyoulove <584703362@qq.com>
2018-05-09 15:58:20 +0800

    test

--- src/main/java/com/haoyoulove/coding/test/ExTest.java
+++ src/main/java/com/haoyoulove/coding/test/ExTest.java
@@ -2,37 +2,82 @@ package com.haoyoulove.coding.test;
 
 import java.util.Collection;
 
+
 public class ExTest {
 
-	private static int j = 0;
+//	private static int j = 0;
+//
+//	private static Boolean methodB(int k) {
+//		j += k;
+//		return true;
+//	}
+//
+//	public static void methodA(int i) {
+//		boolean b;
+//		b = i < 10 | methodB(4);
+//		b = i < 10 || methodB(8);
+//
+//	}
+//
+//	public static void main(String args[]) {
+////		methodA(0);
+////		System.out.println(j);
+//		
+//		String str = "abc";
+//		System.out.println(str.hashCode());
+//		
+//		char c = str.charAt(0);
+//		char c2 = str.charAt(1);
+//		char c3 = str.charAt(2);
+//		System.out.println(0+c);
+//		System.out.println(97 * 31 +c2);
+//		
+//		System.out.println(3105 * 31 +c3);
+//	}
 
-	private static Boolean methodB(int k) {
-		j += k;
-		return true;
+	Node node = new Node();
+			
+	class Node{
+		int a = 10;
 	}
-
-	public static void methodA(int i) {
-		boolean b;
-		b = i < 10 | methodB(4);
-		b = i < 10 || methodB(8);
-
+	
+	public void test(){
+		System.out.println(node.a);
+		Node h = node;
+		test1(h);
 	}
-
-	public static void main(String args[]) {
-//		methodA(0);
-//		System.out.println(j);
-		
-		String str = "abc";
-		System.out.println(str.hashCode());
-		
-		char c = str.charAt(0);
-		char c2 = str.charAt(1);
-		char c3 = str.charAt(2);
-		System.out.println(0+c);
-		System.out.println(97 * 31 +c2);
-		
-		System.out.println(3105 * 31 +c3);
+	
+	public void test1(Node node){
+		node.a = 100;
 	}
-
-
+	
+	public static void main(String[] args) {
+//		ExTest et = new ExTest();
+//		et.test();
+//		System.out.println(et.node.a);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
